74583de1393934694961af4d1422fe61052840a9
Derek <fedellen@pixelpajamastudios.com>
2020-10-19 23:52:50 -0500

    end of stuctured demo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Why hello there React 👋</p>
-  </div>
-)
+const Hello = (props) => {
+
+  return (
+    <div>
+      <p>Hello {props.name}, this is React 🧬</p>
+      <p>You are {props.age} years old</p>
+    </div>
+  )
+
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/fedellen">fedellen</a>
+    </div>
+  )
+}
+
+const App = () => {
+  
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings There 👋</h1>
+      <Hello name="Sam" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Hello name="Douglas" age={74} />
+      <Footer />
+    </div>
+  )
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
