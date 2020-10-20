c40b0e5a93fcbbdefe21e416dee8a7bde490a868
Derek <fedellen@pixelpajamastudios.com>
2020-10-20 12:25:34 -0500

    refactored to utilize arrays

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const exercises = [10, 7, 14]
+const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+
 const Header = () => {
 
   const course = 'Half Stack application development'
@@ -12,23 +15,13 @@ const Header = () => {
   )
 }
 
-const exercises1 = 10
-const exercises2 = 7
-const exercises3 = 14
-const part1 = 'Fundamentals of React'
-const part2 = 'Using props to pass data'
-const part3 = 'State of a component'
-
-// This is clearly not an efficient way of producing
-// parts; would like to return to this when I learn more
-
 const Part = (props) => {
 
   return (
 
     <div>
-      <h2>Course: {props.part}</h2>
-      <p>Exercises: {props.exercise}</p>
+      <h2>Course: {parts[props.part]}</h2>
+      <p>Exercises: {exercises[props.part]}</p>
     </div>
 
   )
@@ -41,11 +34,11 @@ const Content = () => {
     <div>
 
         <hr />
-        <Part part={part1} exercise={exercises1} />
+        <Part part="0" />
         <hr />
-        <Part part={part2} exercise={exercises2} />
+        <Part part="1" />
         <hr />
-        <Part part={part3} exercise={exercises3} />
+        <Part part="2" />
         <hr />
 
     </div>
@@ -55,9 +48,13 @@ const Content = () => {
 
 const Total = () => {
 
+  const sum = "Total number of exercises: " + exercises.reduce( (a, b) => {
+    return a + b;
+  }, 0);
+
   return (
 
-  <h3>Total number of exercises {exercises1 + exercises2 + exercises3}</h3>
+  <h3>{sum}</h3>
 
   )
 }
