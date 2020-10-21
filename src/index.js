2d6395f97d1f64cef27440b808aa16e99a860c1c
Derek <fedellen@pixelpajamastudios.com>
2020-10-21 18:52:39 -0500

    Added statistics to a table

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistic = (props) => {
+  return (
+    <tr>
+      <th>{props.text}</th>
+      <td>{props.value} {props.percent}</td> 
+    </tr>
+  )
+}
+
 const Statistics = (props) => {
 
   const good = props.stats[0]
   const neutral = props.stats[1]
   const bad = props.stats[2]
 
-
   const total = good + neutral + bad
   const average = ((good * 1) + (bad * -1) / total)
   const percentage = good / total * 100 
 
-  console.log(total)
-  console.log(average)
-  console.log(percentage)
-
-
-
-  return (
-    <div>
-      <h2>Our Amazing Statistics</h2>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-      </ul>
-
-      <ul>
-        <li>Total Feedback: {total}</li>
-        <li>Average Feedback: {average}</li>
-        <li>Percentage Positive: {percentage}%</li>
-
-      </ul>
-    </div>
-  )
+  if (total > 0) {
+    return (
+      <table>
+        <thead>
+          <Statistic text="Good: " value={good} />
+          <Statistic text="Neutral: " value={neutral} />
+          <Statistic text="Bad: " value={bad} />
+          <Statistic text="Total Feedback: " value={total} />
+          <Statistic text="Average Feedback: " value={average} />
+          <Statistic text="Percentage Positive: " value={percentage} percent="%" />
+        </thead>
+      </table>
+    )
+  }
+  return (<h2>No feedback has been given</h2>)
 }
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
@@ -55,6 +55,7 @@ const App = () => {
       <Button handleClick={addGood} text="Good" />
       <Button handleClick={addNeutral} text="Neutral" />
       <Button handleClick={addBad} text="Bad" />
+      <h2>Our Amazing Statistics 📈 </h2>
       <Statistics stats={[good, neutral, bad, good + neutral + bad]} />
     </div>
   )
