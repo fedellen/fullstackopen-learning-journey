e63e6fdc37ecddc4799dbfe4a45dbbb8b471622e
Derek <fedellen@pixelpajamastudios.com>
2020-10-22 15:37:10 -0500

    Added Course component and used reduce() on total

--- src/index.js
+++ src/index.js
@@ -2,97 +2,76 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Header = (course) => {
+const Header = ({course}) => <h1>Basic {course}</h1>
 
-  return (
-  
-  <h1>Basic {course.course}</h1>
+const Course = ({course}) => (
+  <div>
+    <Header course={course.name} />
+    <Content part={course.parts} />
+    <hr />
+    <Total sum={course.parts} />
+  </div>
+)
 
+const Part = (props) => (
+
+  <div>
+    <hr />
+    <h2>Part: {props.part.name}</h2>
+    <p>Exercises: {props.part.exercises} 😶</p> 
+  </div>
+)
+
+ // Should refactor to utilize loop
+const Content = (props) => (
+  <div>
+    <Part part={props.part[0]} />
+    <Part part={props.part[1]} />
+    <Part part={props.part[2]} />
+    <Part part={props.part[3]} />
+  </div>
+)
+
+const Total = ({sum}) => {
+
+  const total = sum.reduce((getSum, currentValue) =>
+    getSum + currentValue.exercises, 0
   )
-}
-
-const Part = (props) => {
 
   return (
-
-    <div>
-      <hr />
-      <h2>Course: {props.part.name}</h2>
-      <p>Exercises: {props.part.exercises} 😶</p> 
-    </div>
-  
-  )
-}
-
-
-const Content = (props) => {
-
-    // Should refactor to utilize loop
-
-  return (
-
-    <div>
-      <Part part={props.part[0]} />
-      <Part part={props.part[1]} />
-      <Part part={props.part[2]} />
-    </div>
-
-  )
-}
-
-const Total = (props) => {
-  
-    let exercise = 0
-
-    for (var i = 0; i < props.sum.length; i++) {
-
-        // Loop to add exercise totals together
-      exercise = exercise + props.sum[i].exercises
-
-    }
-
-  return (
-
-    // Display Total
-  <h3>Total number of exercises: {exercise}</h3>
-
+    <h3>Total number of exercises: {total}</h3>
   )
 }
 
 const App = () => {
-
-
   const course = {
-
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Basic Node.js Overview',
+        exercises: 4,
+        id: 4
       }
     ]
   }
 
-  return (
-
-    <div>
-      
-      <Header course={course.name} />
-      <Content part={course.parts} />
-      <hr />
-      <Total sum={course.parts} />
-
-    </div>
-
-  )
+  return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
