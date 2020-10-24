60645a2bbd3bd0768d9f5af80e9cb6627319788f
Derek <fedellen@pixelpajamastudios.com>
2020-10-23 21:37:34 -0500

    Updated to use a JSON database initially

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const SubmissionForm = ({persons, addPerson}) => {
 
@@ -67,14 +68,16 @@ const Filter = ({newFilter, handleFilter}) => (
 
 const App = () => {
 
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',        number: "384-819-5555",   id: 0 },
-    { name: 'Ada Lovelace',       number: '39-44-5323523',  id: 1 },
-    { name: 'Dan Abramov',        number: '12-43-234345',   id: 2 },
-    { name: 'Mary Poppendieck',   number: '39-23-6423122',  id: 3 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
+  const [ newFilter, setNewFilter ] = useState('')
 
-  const [newFilter, setNewFilter] = useState('')
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
 
   const doFilter = (filterValue) => (
