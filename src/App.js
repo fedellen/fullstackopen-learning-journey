eeba46ccd3c8df4e39edef4c4eef62aa3e8e24fe
Derek <fedellen@pixelpajamastudios.com>
2020-10-24 21:24:54 -0500

    Search for countries from API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,98 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const Filter = () => {
 
-  const [ searchValue, newSearchValue ] = useState('')
+const Filter = ({term, search}) => (
+  <div>Find Countries: <input value={term} onChange={search}/></div>
+)
 
-  let typingTimer = {}
+const DisplayCountries = ({showCountries}) => {
 
-  const handleSearch = (search) => {
-    clearTimeout(typingTimer)
-    newSearchValue(search.target.value)
-    if (searchValue) {
-      typingTimer = setTimeout(console.log('hello', searchValue, typingTimer), 750)}
-    
+  const flagStyle = {
+    width: '200px'
   }
 
+  if (showCountries.length > 10) {
+    return (
+      <div>There are too many search results.</div>
+    )
+  }
+
+  if (showCountries.length === 1) {
+    let country = showCountries[0]
+    return(
+      <div>
+        <div>
+          <h1>{country.name}</h1>
+          <p>Capital: {country.capital} </p>
+          <p>Population: {country.population} </p>
+        </div>    
+        <hr />
+        <div>
+          <h3>Languages</h3>
+          <ul>
+            {country.languages.map(language =>
+              <li key={language.name}>{language.name}</li>
+            )}
+          </ul> 
+        </div>
+        <div>
+          <img style={flagStyle} src={country.flag} alt="country-flag" />
+        </div>
+      </div>
+
+    )
+  } 
+  
+  
   return(
-    <div>Find Countries: <input value={searchValue} onChange={handleSearch} /></div>
-  )
+    <div>
+      {showCountries.map(country => 
+        <div key={country.numericCode}>
+          {country.name}
+          <button  />
+        </div>    
+      )}
+    </div>
+   )
+  
+  
+
 }
 
 const App = () => {
   
+  const [ countries, populateCountries ] = useState([])
+  const [ searchTerm, changeTerm ] = useState('')
+  const [ showCountries, filterCountries ] = useState([])
 
+
+  const handleTerm = (term) => {
+    changeTerm(term.target.value)
+    console.log('here it is:', searchTerm)
+    console.log(countries.filter(el => el.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ))
+    filterCountries(countries.filter(el => el.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ))
+    console.log(showCountries, showCountries.length, showCountries.length)
+    
+  } 
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log(response)
+        populateCountries(response.data)
       })
-  })
+    
+  }, [])
+
+
 
   return(
     <div>
-      <Filter />
+      <Filter term={searchTerm} search={handleTerm} />
+      <DisplayCountries showCountries={showCountries} />
+      
+
     </div>
   )
 }
