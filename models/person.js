a7ebddbac5cb91fcaba0ee5a81c78ac267b19be5
fedellen <fedellen@fedellen-ubuntu.lan>
2020-10-30 15:52:58 -0500

    Refactored with eslint

--- models/person.js
+++ models/person.js
@@ -6,24 +6,20 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {    
-    console.log('Server is now connected to MongoDB')  
-  })  
-  .catch((error) => {    
-    console.log('There was an error connecting to MongoDB:', error.message)  
-  })
-  
+  .then(() => console.log('Server is now connected to MongoDB'))
+  .catch((error) => console.log('There was an error connecting to MongoDB: ', error.message))
+
 const personSchema = new mongoose.Schema({
-  name: { 
+  name: {
     type: String,
     required: true,
     unique: true,
-    minlength: 3 
+    minlength: 3
   },
-  number: { 
-    type: String, 
+  number: {
+    type: String,
     required: true,
-    minlength: 8 
+    minlength: 8
   }
 })
 
