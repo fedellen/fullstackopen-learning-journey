c6d14981e3b2247c1f6f55e2c1a200890485b08c
fedellen <fedellen@fedellen-ubuntu.lan>
2020-10-30 16:03:32 -0500

    eslint chages

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-	// I like the log (:
+// I like the log (:
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -42,7 +42,6 @@ app.post('/api/notes', (request, response, next) => {
       response.json(savedAndFormattedNote)
     })
     .catch(err => next(err))
-    
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -59,9 +58,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
+    .then(() => response.status(204).end())
     .catch(error => next(error))
 })
 
@@ -89,7 +86,7 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
@@ -100,7 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- models/note.js
+++ models/note.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {    
-    console.log('connected to MongoDB')  
-  })  
-  .catch((error) => {    
-    console.log('error connecting to MongoDB:', error.message)  
+  .then(() => {
+    console.log('connected to MongoDB')
   })
-  
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 const noteSchema = new mongoose.Schema({
   content: {
     type      : String,
