3932f865d3f6bc0ff6deb41ddfd477ad5064dfda
fedellen <fedellen@fedellen-ubuntu.lan>
2020-10-30 22:41:44 -0500

    Favorite blog function added and jested

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,17 @@ const totalLikes = (blogs) => {
   return likes.reduce((acc, val) => acc + val)
 }
 
+const favoriteBlog = (blogs) => {
+  const bestBlog = blogs.sort((a, b) => b.likes - a.likes) 
+  return {
+    title: bestBlog[0].title,
+    author: bestBlog[0].author,
+    likes: bestBlog[0].likes
+  }
+}
+
 module.exports = {
+  favoriteBlog,
   totalLikes,
   dummy
 }
