9d5409a8897a0f2b57929ecf48e4dd31c0b6c7f5
fedellen <fedellen@fedellen-ubuntu.lan>
2020-10-30 22:17:38 -0500

    totalLikes function created and tested

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  const likes = blogs.map(blog => blog.likes)
+  return likes.reduce((acc, val) => acc + val)
+}
+
 module.exports = {
+  totalLikes,
   dummy
 }
