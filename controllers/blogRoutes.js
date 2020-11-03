abd42e7d019250970621e31800b8280da9576532
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-02 20:33:33 -0600

    Added  login functionaility with blogs

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -1,6 +1,16 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { response } = require('express')
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,20 +19,26 @@ blogRouter.get('/', async (req, res) => {
 
 blogRouter.post('/', async (req, res) => {
   const body = req.body
-  const user = await User.find({})
-  const theUser = user[0]
+  const token = getTokenFrom(req)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'Token is missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
-    user: theUser._id,
+    user: user._id,
     url: body.url,
     likes: body.likes || 0
   })
 
   const savedBlog = await blog.save()
-  theUser.blogs = theUser.blogs.concat(savedBlog._id)
-  await theUser.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   res.status(201).json(savedBlog)
 })
