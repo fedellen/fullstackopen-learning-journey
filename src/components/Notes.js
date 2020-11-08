ae68496e8e733710f6a8936d309effb4980194ac
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-08 13:19:29 -0600

    Redux dev tools are good

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -14,7 +14,14 @@ const Note = ({ note, handleClick }) => {
 const Notes = () => {
 
   const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const notes = useSelector(({ filter, notes }) => {
+    if ( filter === 'ALL' ) {
+      return notes
+    }
+    return filter === 'IMPORTANT'
+      ? notes.filter(note => note.important)
+      : notes.filter(note => !note.important)
+  })
 
   return(
     <ul>
@@ -22,7 +29,8 @@ const Notes = () => {
         <Note 
           key={note.id}
           note={note}
-          handleClick={() => dispatch(toggleImportanceOf(note.id))}
+          handleClick={() => 
+            dispatch(toggleImportanceOf(note.id))}
         />
       )}
     </ul>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()  
+)
+
+store.subscribe(() => console.log(store.getState()))
 
 ReactDOM.render(
-  <Provider store={store}>    <App />
-  </Provider>,  document.getElementById('root')
+  
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
