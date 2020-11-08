6cc372cb20e6e1aca5e9ac6a79a916404c7518d2
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-07 22:34:14 -0600

    Things were done

--- src/index.js
+++ src/index.js
@@ -1,78 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-
+import { Provider } from 'react-redux'
+import App from './App'
 import noteReducer from './reducers/noteReducer'
-
-/*
-  // Redux Counter Example App
-
-import counterReducer from './reducers/counterReducer' 
-
-const store = createStore(counterReducer)
-
-const App = () => {
-
-  return(
-    <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={e => store.dispatch({ type: 'ZERO' })}>
-        zero
-      </button>
-    </div>
-  )
-}
-*/
-
-
-
-
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note =>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>  
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.render(
+  <Provider store={store}>    <App />
+  </Provider>,  document.getElementById('root')
+)
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -11,4 +11,33 @@ const counterReducer = (state = 0, action) => {
   }
 }
 
+
+/*
+  // Redux Counter Example App
+
+import counterReducer from './reducers/counterReducer' 
+
+const store = createStore(counterReducer)
+
+const App = () => {
+
+  return(
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>
+        plus
+      </button>
+      <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>
+        minus
+      </button>
+      <button onClick={e => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+    </div>
+  )
+}
+*/
+
 export default counterReducer
