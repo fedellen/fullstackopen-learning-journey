860850b8ccc9f9e3a7f677393a3a60e9477e0eac
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-07 23:06:50 -0600

    Unused code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
+  
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-
-  }
 
   return (
     <div>
