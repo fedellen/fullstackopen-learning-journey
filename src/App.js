fe808a5741e5c7432dcdbcbb7d7c32a313e3b477
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-09 11:13:01 -0600

    Course progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// Components
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
+// For note initialization from DB
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
 
   return(
     <div>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-import { composeWithDevTools } from 'redux-devtools-extension'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(
-  reducer,
-  composeWithDevTools()  
-)
-
-store.subscribe(() => console.log(store.getState()))
 
 ReactDOM.render(
   
