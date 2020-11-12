4cc304c1453dcb79396fef1c10426cab2db9fce3
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-12 16:40:47 -0600

    Removed redundant import

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import Blog from './Blog'
 import NewBlog from './NewBlog'
 import Togglable from './Togglable'
 
