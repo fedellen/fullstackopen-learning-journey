7bc14efb8b1f0bafca44a909ffb4ea8f48f626bc
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-11 23:19:34 -0600

    Added tests directory

--- src/tests/NewBlog.test.js
+++ src/tests/NewBlog.test.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
-import NewBlog from './NewBlog'
+import NewBlog from '../components/NewBlog'
 
 describe('<NewBlog /> Testing', () => {
-
   let component
   const addBlogHandler = jest.fn()
 
   beforeEach(() => {
-    component = render(
-      <NewBlog addBlog={addBlogHandler} />
-    )
+    component = render(<NewBlog addBlog={addBlogHandler} />)
   })
 
-
   test('NewBlog creates blog with correct details', () => {
-
     const title = component.container.querySelector('#title')
     const author = component.container.querySelector('#author')
     const url = component.container.querySelector('#url')
@@ -36,5 +31,4 @@ describe('<NewBlog /> Testing', () => {
     expect(addBlogHandler.mock.calls[0][0].author).toBe('The Author')
     expect(addBlogHandler.mock.calls[0][0].url).toBe('www.theurl.com')
   })
-
 })
