379c6c788d9fd0e17c821bda493eb7aa60a1d77a
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-17 17:20:22 -0600

    sthing

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -10,16 +10,27 @@ const PersonForm = ({ setError }) => {
   const [city, setCity] = useState('')
 
   const [createPerson] = useMutation(CREATE_PERSON, {
-    refetchQueries: [{ query: ALL_PERSONS }],
     onError: (err) => {
       setError(err.graphQLErrors[0].message)
+    },
+    update: (store, response) => {
+      const dataInStore = store.readQuery({ query: ALL_PERSONS })
+      store.writeQuery({
+        query: ALL_PERSONS,
+        data: {
+          ...dataInStore,
+          allPersons: [...dataInStore.allPersons, response.data.addPerson]
+        }
+      })
     }
   })
 
   const submit = (e) => {
     e.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({
+      variables: { name, street, city, phone: phone.length > 0 ? phone : null }
+    })
 
     setName('')
     setPhone('')
