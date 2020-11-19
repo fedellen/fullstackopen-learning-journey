ad763c7fe8d95394f69b2c24004a5ba699f7462c
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-19 15:42:58 -0600

    stuff was thingsed!

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
@@ -14,14 +15,13 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-  const newDiaryEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment
-  });
-  res.json(newDiaryEntry);
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 export default router;
