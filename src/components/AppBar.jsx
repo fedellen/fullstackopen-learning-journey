5fe734ad409fbd980b333fe17e96ccf8e1bcd3d8
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-23 17:02:58 -0600

    app bar in scrollview

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -8,16 +8,18 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     paddingHorizontal: 10,
-    backgroundColor: theme.colors.primary,
-    flexDirection: 'row'
+    backgroundColor: theme.colors.primary
+    //flexDirection: 'row'
   }
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab name='Repositories' link='/' />
-      <AppBarTab name='Sign In' link='/signin' />
+      <ScrollView horizontal>
+        <AppBarTab name='Repositories' link='/' />
+        <AppBarTab name='Sign In' link='/signin' />
+      </ScrollView>
     </View>
   );
 };
