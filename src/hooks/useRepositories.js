e6af9ca57ebbc61d7e81fe7f32b7e1478e066711
fedellen <fedellen@fedellen-ubuntu.lan>
2020-11-24 12:44:21 -0600

    env setup

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,5 +1,4 @@
 import { useQuery } from '@apollo/react-hooks';
-import { useState, useEffect } from 'react';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
@@ -9,8 +8,6 @@ const useRepositories = () => {
   const { data, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network'
   });
-
-  console.log(data);
 
   if (loading) return 'Loading';
   return data;
